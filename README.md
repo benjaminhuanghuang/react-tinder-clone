9b704f1c177ec7f4fc5cf1eb26effe22f3053e6e
Benjamin Huang <bhuang@rms.com>
2020-07-27 12:58:45 -0700

    create app

---
+++ README.md
@@ -0,0 +1,11 @@
+# Let's Build a TINDER Clone with REACT JS for Beginners
+- https://www.youtube.com/watch?v=DQfeB_FKKkc
+
+
+
+## Steps
+1. Ceate react app and firebase project
+```
+  npx create-react-app tinder-clone
+```
+
---
+++ tinder-clone/src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBxjJIiClkk_lLV8MeoVGzjHTWG9ErNNf4",
+  authDomain: "tinder-clone-a13ba.firebaseapp.com",
+  databaseURL: "https://tinder-clone-a13ba.firebaseio.com",
+  projectId: "tinder-clone-a13ba",
+  storageBucket: "tinder-clone-a13ba.appspot.com",
+  messagingSenderId: "1004499830294",
+  appId: "1:1004499830294:web:b736bd66b4263df11d5737",
+  measurementId: "G-B4DP04W7LW"
+};
+
