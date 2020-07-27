182d2cde9f98c20a5c971488e32cc43ee38b69c0
Benjamin Huang <bhuang@rms.com>
2020-07-27 16:22:48 -0700

    update

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -4,23 +4,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //
 //
 import Header from "./Header";
-import TinderCards from "./TinderCards"
-import SwipeButtons from "./SwipeButtons"
+import TinderCards from "./TinderCards";
+import SwipeButtons from "./SwipeButtons";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <Header />
       <Router>
+        <Header />
         <Switch>
-          <Route path="/">
-            <TinderCards/>
-            <SwipeButtons/>
-          </Route>
           <Route path="/chat">
             <h1>Chat Page</h1>
+          </Route>
+          <Route path="/">
+            <TinderCards />
+            <SwipeButtons />
           </Route>
         </Switch>
       </Router>
--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import PersionIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
@@ -13,15 +14,19 @@ function Header() {
       <IconButton>
         <PersionIcon className="header__icon" fontSize="large" />
       </IconButton>
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo-768x432.png"
+          alt=""
+        />
+      </Link>
 
-      <img
-        className="header__logo"
-        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo-768x432.png"
-        alt=""
-      />
-      <IconButton>
-        <ForumIcon className="header__icon" fontSize="large" />
-      </IconButton>
+      <Link to="/chat">
+        <IconButton>
+          <ForumIcon className="header__icon" fontSize="large" />
+        </IconButton>{" "}
+      </Link>
     </div>
   );
 }
