d785b8da41a44ab17128885fd39de46b499bd27e
Benjamin Huang <bhuang@rms.com>
2020-07-27 13:27:25 -0700

    update header

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from "react";
 
-import PersionIcon from "@material-ui/icons/Person"
-import ForumIcon from "@material-ui/icons/Forum"
+import PersionIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
 
 //
-import "./Header.css"
+import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <PersionIcon/>
-      <h2>Header</h2>
-      <ForumIcon/>
+      <PersionIcon className="header__icon" fontSize="large" />
+      <img
+        className="header__logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo-768x432.png"
+        alt=""
+      />
+      <ForumIcon className="header__icon" fontSize="large" />
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
