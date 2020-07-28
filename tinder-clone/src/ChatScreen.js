f6793d56de46b9dc75596c442873c7bcfd198641
Benjamin Huang <bhuang@rms.com>
2020-07-27 17:20:12 -0700

    upate

--- tinder-clone/src/ChatScreen.js
+++ tinder-clone/src/ChatScreen.js
@@ -7,17 +7,24 @@ function ChatScreen() {
   const [messages, setMessages] = useState([
     { name: "aa", image: "", message: "dddddd" },
     { name: "aa", image: "", message: "dddddd" },
-    { name: "aa", image: "", message: "dddddd" },
+    { image: "", message: "dddddd" },
   ]);
 
   return (
     <div className="chatScreen">
-      {messages.map((message, i) => (
-        <div key={i} className="chatScreen__message">
-          <Avatar className="chat__image" alt={message.name} src={message.image} />
-          <p className="chatScreen__text">{message.message}</p>
-        </div>
-      ))}
+      <p className="chatScreen__timestamp"> You matched with aaa on 10/08/20</p>
+      {messages.map((message, i) =>
+        message.name ? (
+          <div key={i} className="chatScreen__message">
+            <Avatar className="chat__image" alt={message.name} src={message.image} />
+            <p className="chatScreen__text">{message.message}</p>
+          </div>
+        ) : (
+          <div key={i} className="chatScreen__message">
+            <p className="chatScreen__textUser">{message.message}</p>
+          </div>
+        )
+      )}
     </div>
   );
 }
