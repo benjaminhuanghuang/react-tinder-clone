91d6da464d3e58afcd0761d3d19d4d62c863948e
Benjamin Huang <bhuang@rms.com>
2020-07-27 17:23:00 -0700

    update

--- tinder-clone/src/Chat.js
+++ tinder-clone/src/Chat.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import Avatar from "@material-ui/core/Avatar";
 
@@ -6,14 +7,16 @@ import "./Chat.css";
 
 function Chat({ name, message, profilePic, timestamp }) {
   return (
-    <div className="chat">
-      <Avatar className="chat__image" alt={name} src={profilePic} />
-      <div className="chat__details">
-        <h2>{name}</h2>
-        <p>{message}</p>
+    <Link to={`/chat/${name}`}>
+      <div className="chat">
+        <Avatar className="chat__image" alt={name} src={profilePic} />
+        <div className="chat__details">
+          <h2>{name}</h2>
+          <p>{message}</p>
+        </div>
+        <p className="chat__timestamp">{timestamp}</p>
       </div>
-      <p className="chat__timestamp">{timestamp}</p>
-    </div>
+    </Link>
   );
 }
 
